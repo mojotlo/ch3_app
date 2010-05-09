d06db7269f8f3256f196e0fb595c35ae5ca5c716
Taylor Jackson <taylorjackson@taylor-jacksons-macbook-2.local>
2010-05-09 15:56:15 +0200

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about '/about', :controller => 'pages', :action => 'about'
+  map.help '/help', :controller => 'pages', :action => 'help' 
+  map.signup '/signup', :controller => 'users', :action => 'new'
   # The priority is based upon order of creation: first created -> highest priority.
 
-  # Sample of regular route:
+  # Sampleåof regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
 
@@ -31,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "pages" , :action => "home"
 
   # See how all your routes lay out with "rake routes"
 
